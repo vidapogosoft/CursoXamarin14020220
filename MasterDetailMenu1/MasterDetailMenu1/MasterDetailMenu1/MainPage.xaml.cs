3c1ada90767f548bbd17252864a01799dbd90f81
vidapogosoft <vidapogosoft@hotmail.com>
2020-03-21 15:14:11 -0500

    Talleres Dia 2
    Manejo de Hojas de Estilos
    Menus Tabbed/Master Detail
    Camara

---
+++ MasterDetailMenu1/MasterDetailMenu1/MasterDetailMenu1/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MasterDetailMenu1
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            MyMenu();
+
+        }
+
+        private void ListMenu_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var menu = e.SelectedItem as Menu;
+
+            if (menu != null)
+            {
+                IsPresented = false;
+                Detail = new NavigationPage(menu.Page);
+            }
+        }
+
+        public void MyMenu()
+        {
+            Detail = new NavigationPage(new Home());
+            List<Menu> menu = new List<Menu>
+            {
+                new Menu { Page = new Home(), MenuTitle = "Home" },
+                new Menu { Page = new Cupones(), MenuTitle = "Cupones" },
+                new Menu { Page = new Perfil(), MenuTitle = "Perfil" },
+            };
+
+            this.ListMenu.ItemsSource = menu;
+
+        }
+
+        public class Menu
+        { 
+            public string MenuTitle
+            { get; set;}
+
+            public Page Page
+            { get; set; }
+        }
+    }
+}
---
+++ XamarinCamara1/XamarinCamara1/XamarinCamara1/MainPage.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+
+namespace XamarinCamara1
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnTomaFotos_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await CrossMedia.Current.Initialize();
+
+                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+                {
+                    await DisplayAlert("Camara no habilitada", "Revise sus dispositivo", "Cerrar");
+                    return;
+                }
+
+                var file = await CrossMedia.Current.TakePhotoAsync(
+                    new StoreCameraMediaOptions
+                    { 
+                        SaveToAlbum = true,
+                        //PhotoSize = PhotoSize.Medium,
+                        PhotoSize = PhotoSize.Custom,
+                        CustomPhotoSize = 80
+                    }                    
+                    );
+
+                if (file == null)
+                {
+                    await DisplayAlert("Camara","No realizo captura de foto.","Cerrar");
+                    return;
+                }
+
+                this.PathLabel.Text = "la ruta de la foto es: " + file.AlbumPath;
+
+                this.MainImage.Source = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    file.Dispose();
+                    return stream;
+                }
+                );
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "Cerrar");
+            }
+        }
+
+        private void BtnGrabaVideo_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnSubeFoto_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnSubeVideo_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
